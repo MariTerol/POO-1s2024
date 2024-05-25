b90cc32de3b2066733ba7b5631e779fb27f7e696
Mariana Terol <mariananterol@gmail.com>
2024-05-25 09:54:56 -0300

    add files

--- exemplos/exception/Main.java
+++ exemplos/exception/Main.java
@@ -1,9 +1,15 @@
 package exemplos.exception;
 
+import exemplos.exception.banco.Banco;
+
 public class Main {
     
     public static void main(String[] args) {
         
+        Banco UCBank = new Banco();
+
+        UCBank.abrirConta("Conta 1");
+        UCBank.depositar("Conta 1", 10.0);
     }
 
 }
