f3ab15174bd2ea956fe9c28f1aa2225a6d762746
Mariana Terol <111513262+MariTerol@users.noreply.github.com>
2024-05-26 12:14:40 -0300

    Update Main.java

--- exemplos/exception/Main.java
+++ exemplos/exception/Main.java
@@ -13,12 +13,13 @@ public class Main {
         UCBank.abrirConta("Conta 1");
 
         try {
-            UCBank.depositar("Conta 1", 10.0);
-        } catch (ContaIneException e) {
-            System.out.println("Conta Inexistente! Tente novamente.");
+            UCBank.depositar("Conta 2", -10.0);
+        } 
+        catch (ContaIneException e) {
+            System.out.println(e);
         }
         catch (ValorDepositoInvalidoException e) {
-            System.out.println("Valor invalido, tente um numero positivo!");
+            System.out.println(e);
         }
         catch (Exception e) {
             System.out.println("Algo deu errado! Tente novamente.");
